a03c8a69d607aa212b29c4fecf02cd29c699d24d
jackielongofvietnam <jackielongofvietnam@gmail.com>
2023-10-31 20:10:39 +0700

    modify variable

--- Backend_Mac/modules/audit.py
+++ Backend_Mac/modules/audit.py
@@ -6,17 +6,15 @@ def audit_smart_contract(db, file_name, file_content):
 
     #Push audit result to db and store smart contract file
     status = "risky"
-    vulnerabilities = [
-        [
-            {
-                "issue": "Smart contract is risky",
-                "suggestion": "Fix it"
-            },
-            {
-                "issue": "Smart contract is dangerous",
-                "suggestion": "Remove it"
-            }
-        ]
+    vulnerabilities = [ 
+        {
+            "issue": "Smart contract is risky",
+            "suggestion": "Fix it"
+        },
+        {
+            "issue": "Smart contract is dangerous",
+            "suggestion": "Remove it"
+        }       
     ]
     audit_report = insert_audit_report(db, file_name, file_content, status, vulnerabilities)
 
