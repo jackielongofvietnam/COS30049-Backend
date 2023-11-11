36a84270850c91965906aeea80a4440ca9bbd89b
jackielongofvietnam <jackielongofvietnam@gmail.com>
2023-11-11 09:09:17 +0700

    add git ignore and remove some cmd in bat

--- requirements.bat
+++ requirements.bat
@@ -1,11 +1,9 @@
-python3 -m venv .venv_window
-.\.env_window\Scripts\activate
-pip3 install Flask
-pip3 install flask_cors
-pip3 install pymongo
-pip3 install certifi
-pip3 install PyJWT
-pip3 install slither-analyzer
-pip3 install solc-select
+pip install Flask
+pip install flask_cors
+pip install pymongo
+pip install certifi
+pip install PyJWT
+pip install slither-analyzer
+pip install solc-select
 solc-select install 0.5.0
 solc-select use 0.5.0
